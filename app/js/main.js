45893711f185fef1d3765188bb9f589e4f5e55a4
Roman <romahassazhyrov@gmail.com>
2021-02-01 14:20:21 +0300

    fixed unit 12 / added unit 13

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,15 @@ $(function () {
 
     var mixer = mixitup('.products__items');
 
+    var mixer = mixitup('.design__list');
+
+    $('.bottom-slider__carousel').slick({
+        arrows: false,
+        dots: false,
+        speed: 1300,
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
 });
