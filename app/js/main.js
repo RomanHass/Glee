759a8c983aa54abc8b680d3891f804492ba5c5e3
Roman <romahassazhyrov@gmail.com>
2021-02-04 19:57:28 +0300

    fixed icons

--- app/js/main.js
+++ app/js/main.js
@@ -6,19 +6,19 @@ $(function () {
         speed: 1300,
     });
 
-    let mix_1 = document.querySelector('.products__items');
-    let mix_2 = document.querySelector('.design__list');
+    // let mix_1 = document.querySelector('.products__items');
+    // let mix_2 = document.querySelector('.design__list');
 
-    let mixer1 = mixitup(mix_1, {
-        selectors: {
-            control: '.products__btn'
-        }
-    });
-    let mixer2 = mixitup(mix_2, {
-        selectors: {
-            control: '.design__btn'
-        }
-    });
+    // let mixer1 = mixitup(mix_1, {
+    //     selectors: {
+    //         control: '.products__btn'
+    //     }
+    // });
+    // let mixer2 = mixitup(mix_2, {
+    //     selectors: {
+    //         control: '.design__btn'
+    //     }
+    // });
 
 
     $('.bottom-slider__carousel').slick({
