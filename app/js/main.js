f310a2fb825511ebda89d189420cac5ebb78eaab
Roman <romahassazhyrov@gmail.com>
2021-02-08 19:24:51 +0300

    added Adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,64 @@
 $(function () {
 
+// Main slider
     $('.main-slider').slick({
         arrows: false,
         dots: true,
         speed: 1300,
     });
 
-    // let mix_1 = document.querySelector('.products__items');
-    // let mix_2 = document.querySelector('.design__list');
-
-    // let mixer1 = mixitup(mix_1, {
-    //     selectors: {
-    //         control: '.products__btn'
-    //     }
-    // });
-    // let mixer2 = mixitup(mix_2, {
-    //     selectors: {
-    //         control: '.design__btn'
-    //     }
-    // });
-
+//Bottom slider
     $('.bottom-slider__carousel').slick({
         arrows: false,
         dots: false,
         speed: 1300,
         slidesToShow: 5,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+            breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+            breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+            breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+            breakpoint: 425,
+                settings: {
+                    slidesToShow: 1,
+            },
+        }
+        ]
     });
 
-    $('.user__btn--click').on('click', function() {
+// Mixitup
+    let mix_1 = document.querySelector('.products__items');
+    let mix_2 = document.querySelector('.design__list');
+
+    let mixer1 = mixitup(mix_1, {
+        selectors: {
+            control: '.products__btn'
+        }
+    });
+    let mixer2 = mixitup(mix_2, {
+        selectors: {
+            control: '.design__btn'
+        }
+    });
+
+    $('.user__btn--click').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
         $('body').toggleClass('lock');
     })
