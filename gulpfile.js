a04c47bf6d0c893ad06f96c8cee71214be3962b9
Roman <romahassazhyrov@gmail.com>
2021-02-26 12:58:20 +0300

    fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,7 @@ function cleanDist() {
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/**/*.html']).on('change', browserSync.reload);
+    watch('./app/html/**/*.html', fileInclude);
     watch(['app/images/**.svg']);
 }
 
